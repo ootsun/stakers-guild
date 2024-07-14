b520b95dfe2ccb9bf08a2d15fa4cc5cb615d8cc3
ootsun <g.delhaye@wavenet.be>
2024-07-14 02:34:41 +0200

    Remove Scaffold-ETH mentions

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "Stakers Guild",
+  description: "Join the Stakers Guild",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
